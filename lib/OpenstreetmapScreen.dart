bf2b7e67fcfa95982893930a155474862bf02f48
MohamevallMoctar <medvalloper@hotmail.com>
2025-04-05 04:02:55 +0000

    Route

--- lib/OpenstreetmapScreen.dart
+++ lib/OpenstreetmapScreen.dart
@@ -1,13 +1,16 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_map_location_marker/flutter_map_location_marker.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:location/location.dart';
+import 'package:http/http.dart' as http;
 
 class OpenstreetmapScreen extends StatefulWidget {
   const OpenstreetmapScreen({super.key});
-
 
   @override
   State<OpenstreetmapScreen> createState() => _OpenstreetmapScreenState();
@@ -16,62 +19,119 @@ class OpenstreetmapScreen extends StatefulWidget {
 class _OpenstreetmapScreenState extends State<OpenstreetmapScreen> {
   final MapController _mapController = MapController();
 
-  final Location  _location = Location();
+  final Location _location = Location();
   final TextEditingController _locationController = TextEditingController();
   bool isLoading = true;
 
-  LatLng ? _currentLocation ;
-  LatLng ? _destination ;
+  LatLng? _currentLocation;
+  LatLng? _destination;
   List<LatLng> _route = [];
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
     _initializeLocation();
   }
-  Future<void> _initializeLocation() async {
-    if(! await  _checkRequtestPermissions()) return;
-    _location.onLocationChanged.listen(
-      (LocationData locationData){
-      if(locationData.latitude != null && locationData.longitude != null ){
-        setState(() {
 
-          _currentLocation = LatLng(locationData.latitude!, locationData.longitude!);
-          isLoading= false;
+  Future<void> _initializeLocation() async {
+    if (!await _checkRequtestPermissions()) return;
+    _location.onLocationChanged.listen((LocationData locationData) {
+      if (locationData.latitude != null && locationData.longitude != null) {
+        setState(() {
+          _currentLocation =
+              LatLng(locationData.latitude!, locationData.longitude!);
+          isLoading = false;
         });
       }
     });
+  }
 
+  // method to fetch coordinates for a fiven location
+  Future<void> _fetchCoordinatesPoints(String location) async {
+    final url = Uri.parse(
+        "https://nominatim.openstreetmap.org/search?q=$location&format=json&limit=1");
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      if (data.isNotEmpty) {
+        // extract latitude and longitude from API response
+        final lat = double.parse(data[0]['lat']);
+        final lon = double.parse(data[0]['lon']);
+        setState(() {
+          _destination = LatLng(lat, lon);
+        });
+        await _fetchRoute();
+      } else {
+        errorMessage('Location not found . Please tryn another search ');
+      }
+    } else {
+      errorMessage('Failed fetchin location. Try again ');
+    }
   }
 
   Future<bool> _checkRequtestPermissions() async {
     bool serviceEnablled = await _location.serviceEnabled();
-    if(!serviceEnablled){
+    if (!serviceEnablled) {
       serviceEnablled = await _location.requestService();
-      if(!serviceEnablled){
+      if (!serviceEnablled) {
         return false;
       }
     }
     PermissionStatus permissionGranted = await _location.hasPermission();
-    if(permissionGranted == PermissionStatus.denied){
+    if (permissionGranted == PermissionStatus.denied) {
       permissionGranted = await _location.requestPermission();
-      if(permissionGranted != PermissionStatus.granted){
+      if (permissionGranted != PermissionStatus.granted) {
         return false;
       }
     }
     return true;
-
   }
 
-  Future <void> _userCurrentLocation() async {
-    if(_currentLocation != null){
-       _mapController.move(_currentLocation!, 15);
-    }else{
+  Future<void> _userCurrentLocation() async {
+    if (_currentLocation != null) {
+      _mapController.move(_currentLocation!, 15);
+    } else {
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(content: Text("Current location is not available")),
       );
     }
   }
+
+  void errorMessage(String message) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text(message)),
+    );
+  }
+
+  Future<void> _fetchRoute() async {
+    if (_currentLocation == null || _destination == null) return;
+    final url = Uri.parse(
+      "http://router.project-osrm.org/route/v1/driving/"
+      '${_currentLocation!.longitude},${_currentLocation!.latitude};'
+      '${_destination!.longitude},${_destination!.latitude}?overview=full&geometries=polyline',
+    );
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      final geometry = data['routes'][0]['geometry'];
+      _decodePolyline(geometry);
+    } else {
+      errorMessage('Failed to find route. You could try again later ');
+    }
+  }
+
+  // method to decode a polyline string into a list of geographic coordinates
+  void _decodePolyline(String enodedPolyline) {
+    PolylinePoints polylinePoints = PolylinePoints();
+    List<PointLatLng> decodedPoints =
+        polylinePoints.decodePolyline(enodedPolyline);
+    setState(() {
+      _route = decodedPoints
+          .map((point) => LatLng(point.latitude, point.longitude))
+          .toList();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -81,7 +141,6 @@ class _OpenstreetmapScreenState extends State<OpenstreetmapScreen> {
         backgroundColor: Colors.blue,
       ),
       body: Stack(
-
         children: [
           FlutterMap(
             mapController: _mapController,
@@ -91,37 +150,104 @@ class _OpenstreetmapScreenState extends State<OpenstreetmapScreen> {
               minZoom: 0,
               maxZoom: 100,
             ),
-            children:
-              [
-                  TileLayer(
-                    urlTemplate:  'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-                  ),
-                CurrentLocationLayer(
-                  style: const LocationMarkerStyle(
-                    marker: DefaultLocationMarker(
-                      child: Icon(Icons.location_pin, color: Colors.white,),
-
+            children: [
+              TileLayer(
+                urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+              ),
+              CurrentLocationLayer(
+                style: const LocationMarkerStyle(
+                  marker: DefaultLocationMarker(
+                    child: Icon(
+                      Icons.location_pin,
+                      color: Colors.white,
                     ),
-                    markerSize: Size(35,35),
-                    markerDirection: MarkerDirection.heading,
                   ),
-
+                  markerSize: Size(35, 35),
+                  markerDirection: MarkerDirection.heading,
                 ),
-              ],
+              ),
+              if (_destination != null)
+                MarkerLayer(
+                  markers: [
+                    Marker(
+                      point: _destination!,
+                      width: 50,
+                      height: 50,
+                      child: const Icon(
+                        Icons.location_pin,
+                        size: 40,
+                        color: Colors.red,
+                      ),
+                    ),
+                  ],
+                ),
+              if (_currentLocation != null &&
+                  _destination != null &&
+                  _route.isNotEmpty)
+                PolylineLayer(
+                  polylines: [
+                    Polyline(
+                      points: _route,
+                      strokeWidth: 5,
+                      color: Colors.red,
+                    ),
+                  ],
+                ),
+            ],
+          ),
+          Positioned(
+            top: 0,
+            right: 0,
+            left: 0,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      controller: _locationController,
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white,
+                        hintText: 'Enter a location',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide: BorderSide.none,
+                        ),
+                        contentPadding:
+                            const EdgeInsets.symmetric(horizontal: 20),
+                      ),
+                    ),
+                  ),
+                  IconButton(
+                    style: IconButton.styleFrom(
+                        backgroundColor: Colors.white,
+                        foregroundColor: Colors.blue),
+                    onPressed: () {
+                      final location = _locationController.text.trim();
+                      if (location.isNotEmpty) {
+                        _fetchCoordinatesPoints(location);
+                      }
+                    },
+                    icon: const Icon(Icons.search),
+                  ),
+                ],
+              ),
+            ),
           ),
         ],
       ),
-      floatingActionButton: FloatingActionButton(onPressed: (){
-
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
           _userCurrentLocation();
-      },
-      child: Icon(Icons.my_location,
-      size: 30,
-      color: Colors.white,
-      ),
+        },
+        child: Icon(
+          Icons.my_location,
+          size: 30,
+          color: Colors.white,
+        ),
         elevation: 3,
         backgroundColor: Colors.blue,
-
       ),
     );
   }
